38150d32e15d1b2012b0f6b1494c56e35cd26f88
Theophile PACAUD <tpacaud@e1r4p7.42.fr>
2019-04-28 16:15:15 +0200

    Automatic from 42

--- Srcs/Unicodes/ft_print_uni_str.c
+++ Srcs/Unicodes/ft_print_uni_str.c
@@ -6,15 +6,15 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 18:45:46 by fratardi          #+#    #+#             */
-/*   Updated: 2019/04/28 16:11:25 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/04/28 16:15:10 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/ft_printf.h"
 
-size_t	ft_print_uni_str(char *str)
+size_t ft_print_uni_str(char *str)
 {
-	size_t	ret;
+	size_t ret;
 
 	if (!str)
 		return (0);
@@ -24,18 +24,20 @@ size_t	ft_print_uni_str(char *str)
 	return (ret);
 }
 
-int	ft_print_n_uni_str(char *str, int size)
+int ft_print_n_uni_str(char *str, int size)
 {
-	int	ret;
+	int ret;
 
 	if (!str)
 	{
 		ft_print_n_uni_str("(null)", size);
-		return(size);
+		return (size);
 	}
 	if (size < 0)
 		return (0);
 	ret = ft_uni_str_len(str);
+	if (size == 0)
+		size = (int)ret;
 	if (ret)
 		write(1, str, (ret >= size) ? size : ret);
 	return (ret >= size ? size : ret);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 16:07:31 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/04/28 16:11:56 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/04/28 16:13:23 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,64 +17,6 @@
 
 int main(void)
 {
-	char *s1;
-	char *s2;
-
-	//ft_printf("%s + %s = %s\n", s1, s2, ft_addstrings(s1, s2));
-	int i = 1;
-
-/* 	while (i++ != 30)
-	{
-		s1 = ft_strdup("2147483647");
-		s2 = ft_ullitoa(i);
-		ft_adjustpospo(&s1, &s2);
-		ft_printf("{%s + %s} = ", s1, s2);
-		ft_putendl(ft_addstrings(s1, s2));
-	} */
-	//ft_floatt(1.1754945E-38);
-	//ft_double(1.1754945E-38);
-	//printf("Vrai__>> %.250f\n", 1.1754945E-38);
-
-	//ft_printf("%s __ %u\n", ft_mantissaldouble(1.1754945E-38),ft_strlen(ft_mantissaldouble(1.1754945E-38)));
-	//ft_hexfloat(&a, sizeof(long double));
-	//printf("exp_ldouble %d\n", ft_expldouble((long double)2.1));
-	//ft_ldouble(1.17549);
-	//ft_printf("Nous__>> %f\n", 3.753);
-	//ft_printf("Rounding = %s\n", ft_rounding(ft_strdup("395"), 4));
-//	ft_printf("%hhd\n", 126);
-//	ft_printf("return = %s %c %lu %d %.3f %X %lld\n", "test", 50, ft_printf("-->%#5X\n", 12), 150, 12.45, 12, (long long int)-2147483648);
-//	printf("REAL = %s %c %lu %d %.3f %X %lld\n", "test", 50, ft_printf("-->%#5X\n", 12), 150, 12.45, 12, (long long int)-2147483648);
-	ft_putnbr(ft_printf("\n%.3s\n", NULL));
-	ft_putnbr(printf("\n%.3s\n", NULL));
-//	ft_floatEdisp("0.0000000541", 2);
-	//ft_printf("Bin double >> %s\n", ft_mantissadouble((double)1.1754945E-38));
-	//ft_printf("Rounding for %d__%s\n", 2, ft_rounding(ft_floatt(1.5656), 2));
-	//ft_printf("Bin = %s\n", ft_bin(2147483647, 64));
+	ft_printf("")
 	return (0);
 }
-
-
-	/* 	while (po != 50)
-	{
-		//ft_printf("pow5 = %s\n", ft_pow5(po));
-		//ft_printf("pow2neg = 0.%s\n", ft_pow2neg(po));
-		po++;
-	} */
-/* 	printf(".%s _ NOUS\n", ft_floatt(3.1));
-	printf("%.80f _ VRAI\n", 3.1); */
-
-	/*ft_fillbig(s1, s2);
-	printf("add - {%s}\n", ft_addstrings(s1, s2)); */
-/* 	int pow = 11;
-	
-	ft_putendl(ft_addstrings(s1, s2));
-	printf("pow = 0.%s\n",ft_decimal(15.4375)); */
-
-	/* Fichier TESTS */
-	/* 	ft_putendl("\n\n\nTESTS COMPLET - <Return>");
-	getchar();
-	tests(); */
-	//while(1);
-
-		/* Fonction de DEBUG */
-	//ft_debug("Something Happen %d %d %d\n", 11, 10, 9);
