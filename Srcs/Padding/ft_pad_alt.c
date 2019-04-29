aca7065f11d9aa5718a80f298dbcb73b97fe118a
Francois TARDIEU <fratardi@e2r10p2.42.fr>
2019-04-29 22:03:36 +0200

    padding avance

--- Srcs/Padding/ft_pad_alt.c
+++ Srcs/Padding/ft_pad_alt.c
@@ -6,7 +6,7 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 20:41:16 by fratardi          #+#    #+#             */
-/*   Updated: 2019/04/29 21:49:17 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/04/29 22:03:09 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 void	ft_pad_alt(t_printinfo list, t_elem elem)
 {
-	if(list.t == 'x' || list.t == 'X')
-		ft_remove_trailing_zeroe(list.buf);
-	if(list.t == 'x')
-		list.buf = ft_strjoinfree(ft_strdup("0x"), list.buf);
-	if(list.t == 'X')
-		list.buf = ft_strjoinfree(ft_strdup("0X"), list.buf);
+		if(list.t == 'x' || list.t == 'X') 
+			ft_remove_trailing_zeroe(list.buf);
+		if(list.t == 'x' && !list.extra)
+			list.buf = ft_strjoinfree(ft_strdup("0x"), list.buf);
+		if(list.t == 'X' && !list.extra)
+			list.buf = ft_strjoinfree(ft_strdup("0X"), list.buf);
 }
