3745e584d37f9944368a4ed9b86a87e44e840f06
Francois TARDIEU <fratardi@e2r1p11.42.fr>
2019-05-08 20:09:49 +0200

    edition du .h et modification de l'ordre des pads .

--- Srcs/Padding/ft_pad.c
+++ Srcs/Padding/ft_pad.c
@@ -6,7 +6,7 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 20:37:18 by fratardi          #+#    #+#             */
-/*   Updated: 2019/05/08 20:00:04 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/05/08 20:05:26 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ void	ft_pad(t_printinfo *info)
 	while(tmp_info->next)
 	{
 		ft_pad_alt(tmp_info);	
-		ft_pad_space(tmp_info)
+		//ft_pad_space(tmp_info);
 		//ft_pad_left(tmp_info);	
 		//ft_pad_showsig(*tmp_info);	
 		//ft_pad_group(tmp_info);
-		ft_pad_extra(tmp__info);
+		ft_pad_extra(tmp_info);
 		tmp_info = tmp_info->next;			
 	}
 }
