e74cb9182f8c6c91cf9f831d849e069ccd9c8841
Francois TARDIEU <fratardi@e1r6p11.42.fr>
2019-05-11 20:33:02 +0200

    ok pad space

--- Srcs/Padding/ft_pad_space.c
+++ Srcs/Padding/ft_pad_space.c
@@ -6,7 +6,7 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 20:42:57 by fratardi          #+#    #+#             */
-/*   Updated: 2019/05/10 23:34:03 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/05/11 20:32:45 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,24 @@
 
 void	ft_pad_space(t_printinfo *list)
 {
-	 int 	width;
-	 int 	blen;
+	int 	width;
+	int 	blen;
 
 	width = list->width;
 	blen = list->buflen;
 	if(!list->space)
 		return;
-	if(ft_strchr(list->buf , '-') && (width == -2 || !list->space)  )
+	if(ft_strchr(list->buf , '-') && (width == -2 || !list->space))
 		return;
-	if(ft_strchr("aAdeEfFgGi",list->t) && ((width > blen) ))
+	if(ft_strchr("aAdeEfFgGi",list->t) && ((width > blen)))
 	{
-		
+
 		list->buf = ft_strjoinfree(ft_memaset(' ', width - blen) , list->buf);
 		list->buflen = width;
-	/*	
-	 *
-	 *	ft_putchar('\n');
-		ft_putnbr(list->buflen);
-		ft_putchar('\n');
-		*/
 	}
-	if(!list->prec)
+	if(list->width == -2)
+	{
 		list->buf = ft_strjoinfree(ft_strdup(" "), list->buf);
+		list->buflen += 1;
+	}
 }
