614d501c82a65f1f283b941690feb64d2bcb7362
tpacaud <xnjustx@gmail.com>
2019-05-18 03:08:21 +0200

    Modifs Double

--- Srcs/Maths/annex_ldouble.c
+++ Srcs/Maths/annex_ldouble.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 16:08:12 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/05/18 03:00:02 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/05/18 03:07:39 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 **Get binary mantissa from long double
 */
 
-char	*ft_mantissaldouble(long double d)
+char *ft_mantissaldouble(long double d)
 {
-	__uint128_t		*temp;
-	char			*f;
-	__uint128_t		i;
-	__uint128_t		pos;
-	__uint128_t		mask;
+	__uint128_t *temp;
+	char *f;
+	__uint128_t i;
+	__uint128_t pos;
+	__uint128_t mask;
 
 	i = -1;
 	f = (char *)ft_memalloc(sizeof(char) * 66);
@@ -44,9 +44,9 @@ char	*ft_mantissaldouble(long double d)
 **Get exp from long double
 */
 
-int		ft_expldouble(long double a)
+int ft_expldouble(long double a)
 {
-	__uint128_t		*b;
+	__uint128_t *b;
 
 	b = (__uint128_t *)&a;
 	*b = *b >> 64;
@@ -56,13 +56,36 @@ int		ft_expldouble(long double a)
 	return ((long long int)(*b) - 16383);
 }
 
+t_double	ft_doublesign(t_double dble)
+{
+	dble.temp = ft_pow2str(dble.b + dble.ex);
+	if (dble.b + dble.ex >= 0)
+	{
+		ft_adjustpospo(&dble.temp, &dble.ent);
+		dble.tempent = ft_addstrings(dble.temp, dble.ent);
+		free(dble.ent);
+		dble.ent = ft_strdup(dble.tempent);
+		free(dble.tempent);
+	}
+	else if (dble.b + dble.ex < 0)
+	{
+		ft_adjustnegpo(&dble.temp, &dble.dec);
+		dble.tempdec = ft_addstrings(dble.temp, dble.dec);
+		free(dble.dec);
+		dble.dec = ft_strdup(dble.tempdec);
+		free(dble.tempdec);
+	}
+	free(dble.temp);
+	return (dble);
+}
+
 /*
 **Main function to calculate and transform mant&exp to str long double
 */
 
-char	*ft_ldouble(long double a, size_t prec)
+char *ft_ldouble(long double a, size_t prec)
 {
-	t_double	dble;
+	t_double dble;
 	int i;
 
 	i = 0;
@@ -74,26 +97,7 @@ char	*ft_ldouble(long double a, size_t prec)
 	while (dble.m[i])
 	{
 		if (dble.m[i] == '1')
-		{
-			dble.temp = ft_pow2str(dble.b + dble.ex);
-			if (dble.b + dble.ex >= 0)
-			{
-				ft_adjustpospo(&dble.temp, &dble.ent);
-				dble.tempent = ft_addstrings(dble.temp, dble.ent);
-				free(dble.ent);
-				dble.ent = ft_strdup(dble.tempent);
-				free(dble.tempent);
-			}
-			else if (dble.b + dble.ex < 0)
-			{
-				ft_adjustnegpo(&dble.temp, &dble.dec);
-				dble.tempdec = ft_addstrings(dble.temp, dble.dec);
-				free(dble.dec);
-				dble.dec = ft_strdup(dble.tempdec);
-				free(dble.tempdec);
-			}
-			free(dble.temp);
-		}
+			dble = ft_doublesign(dble);
 		dble.b--;
 		i++;
 	}
