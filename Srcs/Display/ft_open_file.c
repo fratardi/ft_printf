c9219ffa17f924255251961e6c35f72a3a3c37e7
Francois TARDIEU <fratardi@e1r8p16.42.fr>
2019-05-23 04:08:23 +0200

    display

--- Srcs/Display/ft_open_file.c
+++ Srcs/Display/ft_open_file.c
@@ -6,30 +6,30 @@
 /*   By: fratardi <fratardi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 18:36:25 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/05/23 03:22:56 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/05/23 04:05:26 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/ft_printf.h"
 
-char   *ft_of(char *name)
+char*ft_of(char *name)
 {
-    int         fd;
-    static char buf[2049];
-    char        *ret;
+	int			fd;
+	static char	buf[2049];
+	char		*ret;
 
-    ret = NULL;
-    if ((fd = open(name, O_RDONLY)) == -1)
+	ret = NULL;
+	if ((fd = open(name, O_RDONLY)) == -1)
 		return (NULL);
-    ft_bzero(buf, 2048);
-    if (read(fd, buf, 2048))
-        ret = ft_strdup(buf);
-    ft_bzero(buf, 2048);
-    while (read(fd, buf, 2048))
-    {
-        ret = ft_joinfree(ret, ft_strdup(buf));
-        ft_bzero(buf, 2048);
-    }
-    close(fd);
-    return (ret);
+	ft_bzero(buf, 2048);
+	if (read(fd, buf, 2048))
+		ret = ft_strdup(buf);
+	ft_bzero(buf, 2048);
+	while (read(fd, buf, 2048))
+	{
+		ret = ft_joinfree(ret, ft_strdup(buf));
+		ft_bzero(buf, 2048);
+	}
+	close(fd);
+	return (ret);
 }
