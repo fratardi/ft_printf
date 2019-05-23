37d6434b4e384ec723e685639b9adde7a68293cd
Theophile PACAUD <tpacaud@e1r9p14.42.fr>
2019-05-23 05:05:41 +0200

    Fix_%#X

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 16:07:31 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/05/23 04:43:59 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/05/23 04:57:52 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,6 +57,8 @@ int main(void)
 	ft_printf("%b\n", nb);
 	ft_printf("%F\n", "k");
 
+	ft_printf("m |%#0X|\n", 842);  	
+	printf("r |%#0X|\n", 842);  
 
 	return (0);
 }
