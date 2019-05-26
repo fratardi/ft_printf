9fcdddd1144c1cb6b94ef5a7fd20372edc0a9394
Francois TARDIEU <fratardi@e1r3p17.42.fr>
2019-05-27 00:38:29 +0200

    suggestion pourle handle des 5 ^ n sur strings improved

--- Srcs/Maths/new_addstring.c
+++ Srcs/Maths/new_addstring.c
@@ -1,41 +1,56 @@
 #include "../../Includes/ft_printf.h"
 
-//fonction qui ecrit dans base de content  
 void    ft_addstrings(char *assign, char *base, size_t po_assig, size_t po_base)
 {
-    if(!assign[po_assig]|| !base[po_base])
-        return ;
-    if(((assign[po_assig] - '0') + (base[po_base] - '0') < 9))
-    {
-
-        assign[po_assig] = assign[po_assig] + (base[po_base] - '0');
-        ft_addstrings(assign, base, --po_assig , --po_base);
-    }
-    else
-    {
-        assign[po_assig] = assign[po_assig] - (base[po_base] - '0');
-        assign[po_assig - 1]++;
-        ft_addstrings(assign, base, --po_assig , --po_base);
-    }
-    return ;
+	if(!assign[po_assig]|| !base[po_base])
+		return ;
+	if(((assign[po_assig] - '0') + (base[po_base] - '0') < 9))
+	{
+		assign[po_assig] = assign[po_assig] + (base[po_base] - '0');
+		ft_addstrings(assign, base, --po_assig , --po_base);
+	}
+	else
+	{
+		assign[po_assig] = assign[po_assig] - (base[po_base] - '0');
+		assign[po_assig - 1]++;
+		ft_addstrings(assign, base, --po_assig , --po_base);
+	}
+	return ;
 }
 
-void    ft_neg_pow_of_two(t_list *base, size_t po)
-{
-    char *assign;
-    size_t  i;
-    size_t  j;
 
-    i = base->content_size;
-    if( !po || po <= i || !(assign = (char *)ft_memaset('0', po)))
-        return ;
-    while(i++ < po)
-    {
-        j = 0;
-        while (++j <= 5)
-            ft_addstrings(assign ,base->content, po, base->content_size);
-    }
-    free (base->content);
-    base->content = assign;
-    return ;
+void	ft_init_base(t_list *base)
+{
+	base->content = ft_strdup("5");
+	base->content_size = 1;
+	base->next = NULL;
+}
+
+void	ft_neg_pow_of_two(t_list *base, size_t po)
+{
+	char	*assign;
+	size_t	i;
+	size_t	j;
+
+	i = base->content_size;
+	if( !po || po <= i || !(assign = (char *)ft_memaset('0', po)))
+		return ;
+	while(i++ < po)
+	{
+		j = 0;
+		while (++j <= 5)
+			ft_addstrings(assign ,base->content, po, base->content_size);
+	}
+	free (base->content);
+	base->content = assign;
+	return ;
+}
+
+char	*ft_pow2str(int ex, char end)
+{
+	static t_list base = ft_init_base(&base);
+
+	if (ex == 0)
+		return (ft_strdup("1"));
+	return ((ex > 0) ? ft_pow2c(ex) : ft_neg_pow_of_two(&base, (size_t)-ex);
 }
