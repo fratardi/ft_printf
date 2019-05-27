50692817b1a8e7fda406ff8bd7af5e0f1df5aacb
tpacaud <xnjustx@gmail.com>
2019-05-27 18:52:05 +0200

    Main_Bonus

--- main.c
+++ main.c
@@ -6,14 +6,29 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 23:41:15 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/05/27 15:51:06 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/05/27 18:50:15 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./Includes/ft_printf.h"
 
+void	bonus(void)
+{
+	ft_printf("n$ - inverse = %2$s %1$s\n", "1st_String", "2nd_String");
+	ft_printf("Scientific = %Ef\n", 1.234567E-100);
+	ft_printf("Binary (int) = %b\n", 1);
+	ft_printf("Binary (str){hello} = %B\n", "hello");
+	ft_printf("Octal Upper = %O\n", 42);
+	ft_printf("Unsigned Upper = %U\n", 42);
+	ft_printf("size_t = %zu\n", (size_t)42);
+	ft_printf("Unicodes = 𝟜𝟚 et %s\n", "😀");
+}
+
+
 int main(void)
 {
-	ft_printf("%.50f", 1E-38);
+	bonus();
+	// ft_printf("<> %Lf\n", 2.0E-4930L);
+	// printf(">> %.5000Lf\n", 2.0E-4930L);
 	return (0);
 }
