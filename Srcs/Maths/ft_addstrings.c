00a49843d7d4d17ce73f72783ad5b8737d8d9570
tpacaud <xnjustx@gmail.com>
2019-05-27 18:16:58 +0200

    opti_Floats

--- Srcs/Maths/ft_addstrings.c
+++ Srcs/Maths/ft_addstrings.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 14:31:12 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/05/27 15:50:38 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/05/27 16:32:31 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,28 +81,23 @@ char	*ft_new_addstrings(char *s1, char *s2)
 	int		s2p;
 	int		y;
 	char	diff;
-	char	ret;
+	char	*ret;
 
 	s1p = ft_strlen(s1);
 	s2p = ft_strlen(s2);
+	ret = s1;
 	diff = 0;
 	y = (s1p >= s2p) ? s1p : s2p;
 	while (s1p-- && s2p-- && y--)
-	{
-		diff = ft_addchar(&s1[s1p], &s2[s2p], &ret, diff);
-		s1[s1p] = ret;
-	}
-	s1[s1p] = (y == 0 && diff) ? '1' : '0';
+		diff = ft_addchar(&s1[s1p], &s2[s2p], &ret[y], diff);
+	ret = (y == 0 && diff) ? ft_joinfree(ft_strdup("1"), ret) : ret;
 	if (y == 0)
-		return (s1);
+		return (ret);
 	s2p--;
 	y -= (s2p) ? 1 : 0;
 	while (s1p >= 0 || s2p >= 0)
-	{
-		diff = ft_addchar("0", (s1p >= 0) ? &s1[s1p] : &s2[s2p--],
-		&ret, diff);
-		s1[s1p--] = ret;
-	}
-	s1[s1p] = (diff) ? '1' : '0';
-	return (s1);
+		diff = ft_addchar("0", (s1p >= 0) ? &s1[s1p--] : &s2[s2p--],
+		&ret[y--], diff);
+	ret = (diff) ? ft_joinfree(ft_strdup("1"), ret) : ret;
+	return (ret);
 }
