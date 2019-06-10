d87f87a2c39257a4f019b5ccd031c94102a476d6
Francois TARDIEU <fratardi@e3r2p19.42.fr>
2019-06-10 22:28:53 +0200

    norm ok floats ok miss ormalized values test

--- Srcs/Padding/ft_padding_display.c
+++ Srcs/Padding/ft_padding_display.c
@@ -6,7 +6,7 @@
 /*   By: fratardi <fratardi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/08 03:12:49 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/06/10 22:13:04 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/06/10 22:24:53 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ int		ft_padding_display(t_printinfo *l)
 
 	zero = (l->buf[0] == '0' && !l->buf[1]) ? 1 : 0;
 	ft_pad_prec(l);
+	if (l->t == 'f' && (!ft_strcmp(l->buf, "nan") || !ft_strcmp(l->buf, "inf")
+		|| !ft_strcmp(l->buf, "-inf")))
+		return (ft_pad_float(l));
 	if (ft_strchr("diuUf", l->t))
 		return (ft_pad_di(l));
 	if (ft_strchr("xXoOp", l->t))
 		return (ft_pad_xo(l, zero));
-	if (l->t == 'f' && (!ft_strcmp(l->buf, "nan") || !ft_strcmp(l->buf, "inf")
-		|| !ft_strcmp(l->buf, "-inf")))
-		return (ft_pad_char_type(l));
 	if (l->t == 'c' || l->t == 's')
 		return (ft_pad_char_type(l));
 	if (l->t == 'f' && l->prec == 0 && l->showsign && !l->alt && !l->extra)
