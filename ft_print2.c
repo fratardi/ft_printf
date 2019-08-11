17b8625cea8ab8e0ff30f58f9f21da0fdf19abf7
Theophile PACAUD <tpacaud@e1r2p1.42.fr>
2019-08-11 22:51:39 +0200

    0.2_Norm_Degrossi

--- ft_print2.c
+++ ft_print2.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/11 22:49:38 by tpacaud           #+#    #+#             */
+/*   Updated: 2019/08/11 22:51:28 by tpacaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "includes/ft_printf.h"
 #include <stdlib.h>
 #include <stdarg.h>
 
-t_rep	ft_init_rep(void)
+t_rep		ft_init_rep(void)
 {
 	t_rep rep;
 
@@ -11,20 +23,20 @@ t_rep	ft_init_rep(void)
 	rep.syntaxlen = 0;
 	rep.strpos = 0;
 	rep.vapos = 1;
-	// rep.start = NULL;
-	// rep.current = rep.start;
 	return (rep);
 }
 
-
-int		ft_print_rest(char *seg, int size)
+int			ft_print_rest(char *seg, int size)
 {
 	if (size > 0)
 		ft_putnstr(seg, size);
 	return (size);
 }
 
-//cut segment
+/*
+**cut segment
+*/
+
 int			ft_syntaxlen(const char *format)
 {
 	int i;
@@ -49,17 +61,18 @@ int			ft_sequencelen(const char *format)
 	return (i);
 }
 
-//tanslate segment given by ft_segment to printinfo data type
-t_printinfo  seg_to_print_info(char *seg, t_rep *rep)
+/*
+**tanslate segment given by ft_segment to printinfo data type
+*/
+
+t_printinfo	seg_to_print_info(char *seg, t_rep *rep)
 {
-	t_printinfo list;
-	char *temp;
+	t_printinfo	list;
+	char		*temp;
 
 	temp = ft_strndup(seg, rep->syntaxlen);
-	// printf("syn={%s}\n", temp);
 	ft_fillzerolist(&list);
 	ft_fillndol(temp, &list);
-	//ndol selon precedant ou ndol soit precise
 	ft_fillprec(temp, &list);
 	ft_fillflag(temp, &list);
 	ft_fillwidth(temp, &list);
@@ -71,13 +84,16 @@ t_printinfo  seg_to_print_info(char *seg, t_rep *rep)
 	return (list);
 }
 
-//print the data according to it's printinfo
+/*
+** print the data according to it's printinfo
+*/
+
 size_t		ft_print_seg(const char *format, t_rep *rep)
 {
-	char *seg;
-	char *syntax;
-	t_printinfo info;
-	size_t ret;
+	char		*seg;
+	char		*syntax;
+	t_printinfo	info;
+	size_t		ret;
 
 	ret = 0;
 	seg = (char *)&format[rep->strpos];
@@ -90,21 +106,13 @@ size_t		ft_print_seg(const char *format, t_rep *rep)
 		syntax = ft_fillbuf(&info, rep);
 		syntax = ft_pad_prec(&info, syntax);
 		ret += ft_padding_display(&info, &syntax);
-/* 		if (ft_strchr("fdi", info.t))
-			ft_pad_di(&info, syntax);
-		else
-			ft_putstr(syntax); */
-		// free(syntax);
 		ret += ft_print_rest(&seg[rep->syntaxlen], rep->seglen - rep->syntaxlen);
-		//ft_display(info, rep);
 	}
 	else if (seg[0] == '%')
 	{
 		ret += ft_sequence(seg, rep->seglen);
 		if (seg[rep->seglen] == '%' && seg[rep->seglen] && !ft_issyntax(&seg[rep->seglen], ft_sequencelen(&seg[rep->seglen])))
 			rep->seglen += ft_sequencelen(&seg[rep->seglen]);
-		// ft_printf("{%s}", seg);
-		/* ft_putstr("not_good_syntax"); */
 	}
 	else
 		ret += ft_print_rest(&seg[rep->syntaxlen], rep->seglen - rep->syntaxlen);
@@ -112,10 +120,10 @@ size_t		ft_print_seg(const char *format, t_rep *rep)
 	return (ret);
 }
 
-int     ft_printf(const char *format , ...)
+int			ft_printf(const char *format, ...)
 {
-	t_rep rep;
-	int ret;
+	t_rep	rep;
+	int		ret;
 
 	ret = 0;
 	if (format == NULL)
@@ -123,13 +131,8 @@ int     ft_printf(const char *format , ...)
 	rep = ft_init_rep();
 	va_start(rep.start, format);
 	va_copy(rep.current, rep.start);
-	while(format[rep.strpos])
-	{
-		// printf(">>{%s}\n", &format[rep.strpos]);
+	while (format[rep.strpos])
 		ret += ft_print_seg(format, &rep);
-	}
-	// ret = rep.ret;
 	va_end(rep.start);
-	//ft_free_rep(rep);
-	return(ret);
+	return (ret);
 }
