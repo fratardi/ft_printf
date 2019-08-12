5c7560f6a524d9c801ea9d4e96f14fa111c8bd51
Theophile PACAUD <tpacaud@e1r3p3.42.fr>
2019-08-12 03:41:00 +0200

    0.2_Fix_Hash_octal

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/31 14:03:52 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/08/12 03:23:56 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/08/12 03:34:17 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main(void)
 		// ft_printf("%10s %15d", "test", 1);
 		    // ft_printf("%10s %15d %2$*.*o %X %2$o %5$2.*f %u %p\n", "test", 0x1, 2, 3, 4, 12.54f, 19, "test");
 		    // printf("%10s %15d %2$*.*o %X %2$o %5$.*f %u %p", "test", 0x1, 2, 3, 4, 12.54f, 19, "test");
-		ft_printf("{%-05d}\n", -42);
+		ft_printf("{%#.o}\n", 0);
         // ft_printf("%2$d %3$*.")
 		//printf("%*.*f\n");
     // printf("_%d_", ft_prin \tf("%17.9d et %% et %0$u %20s et %12y %1.4f %O %U", 42, "string",1.2f, 42, 42));
