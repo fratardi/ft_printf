53a589b4cf3523f50cba4876bf55a0c7198a8e41
Theophile PACAUD <tpacaud@e1r3p2.42.fr>
2019-08-13 05:09:50 +0200

    0.2_special_undefined_percent

--- srcs/parsing/ft_sequence.c
+++ srcs/parsing/ft_sequence.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 02:07:09 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/08/12 06:08:05 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/08/13 05:09:33 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,10 @@ size_t		ft_sequence(char *s, int len)
 	{
 		if (s[i + ((sy.left) ? 1 : 0)] == '%')
 			sy.ret += ft_print_n_uni_str(&s[i + ((sy.left) ? 1 : 0)], rest);
-		else
+		else if (len - i)
 			sy.ret += ft_print_n_uni_str(&s[i + ((sy.left) ? 1 : 0)], len - i);
+		else if (rest - 1 > 0)
+			sy.ret += ft_print_n_uni_str(&s[i + ((sy.left) ? 1 : 0)], rest - 1);	
 	}
 	else if (s[len] != 0)
 		sy.ret += ft_print_uni_str((sy.left) ? &s[len + 1] : &s[len]);
