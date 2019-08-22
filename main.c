54e43069073889de13220d24f21f5dd53f9a7751
Francois TARDIEU <fratardi@e1r10p1.42.fr>
2019-08-22 06:20:03 +0200

    main.c

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ t_print_info ft_print_sequence(const char *format, size_t pos)
 	}
 	*/
 //	ft_putnbr(atoi("+-4556"));
-ft_printf("%/$+603.4s", "un", "deux", "trois", "quatre", "cinq");
+	printf("%+3+719-5$-603.4s", "un", "deux", "trois", "quatre", "cinq");
 
 
 
