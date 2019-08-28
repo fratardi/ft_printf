ec111c633b5789f0bb9fb2f47b86c40d387a8059
tpacaud <xnjustx@gmail.com>
2019-08-28 18:58:41 +0200

    Revert "Checker_Bonus&Couleurs  'n'pour_next_specifier_test"
    This reverts commit 102526be000388bb18d6394fa2435433726a7a72.

--- main_debug.c
+++ main_debug.c
@@ -6,7 +6,7 @@
 /*   By: tpacaud <tpacaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/26 01:14:10 by tpacaud           #+#    #+#             */
-/*   Updated: 2019/08/28 18:54:11 by tpacaud          ###   ########.fr       */
+/*   Updated: 2019/08/28 04:13:29 by tpacaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,68 +19,14 @@
 #define LEAKS while(1);
 #endif
 
-void	bonus(void)
-{
-    long double		nb;
-
-	printf("============== NDOL & WILDCARD ===================\n\n");
-	ft_printf("Y> n$ - inverse = %2$s %1$s\n", "1st_String", "2nd_String");
-	printf("R> n$ - inverse = %2$s %1$s\n", "1st_String", "2nd_String");
-	ft_printf("Y> wildcard = %*.*f\n", 26, 18, 123.4f);
-	printf("R> wildcard = %*.*f\n", 26, 18, 123.4f);
-
-	printf("\n\n============== PERSONNAL ===================\n\n");	
-	ft_printf("Unicodes = 4️⃣ 2️⃣  et %s\n", "😀 😝 🖥  🖱 ");
-	ft_printf("Scientific = %Ef\n", 1.234567E-100);
-	ft_printf("Binary int - %b\n", 1);
-	ft_printf("Binary int - %b\n", 2147483647);
-	ft_printf("Binary Sting - %B\n", "2147483647");
-
-	printf("\n\n============== More specifier & converter ===================\n\n");	
-	ft_printf("Y> Octal Upper = %O\n", 42);
-	printf("R> Octal Upper = %O\n", 42);
-	ft_printf("Y> Unsigned Upper = %U\n", 42);
-	printf("R> Unsigned Upper = %U\n", 42);
-	printf("Y> size_t = %zu\n", (size_t)42);
-	ft_printf("R> size_t = %zu\n", (size_t)42);
-
-	printf("\n\n============== Float Precision ===================\n\n");	
-	ft_printf("Your >> %.900Lf\n", 123.456E-250L);
-	printf("Real >> %.900Lf\n\n", 123.456E-250L);
-	ft_printf("Your >> %.20Lf\n", 111111111111111111111111111111111111.22222222222222222222222222222222L);
-	printf("Real >> %.20Lf\n", 111111111111111111111111111111111111.22222222222222222222222222222222L);
-
-	printf("\n\n============== Float Exception ===================\n\n");	
-	nb = 1.0L/0.0L;
-	ft_printf("Y> %5.9Lf\n", nb);
-	printf("R> %5.9Lf\n", nb);
-    nb = -1.0/0.0;
-	ft_printf("Y> %-12Lf\n", nb);
-	printf("R> %-12Lf\n", nb);
-	nb = 0.0L/0.0L;
-	ft_printf("Y> %12Lf\n", nb);
-	printf("R> %12Lf\n", nb);
-	nb = -0.0L;
-	ft_printf("Y> %Lf\n", nb);
-	printf("R> %Lf\n", nb);
-
-	printf("\n\n\n\n============== UNDEFINED ===================\n\n");
-	ft_printf("Y> %88-d\n", 12);
-	printf("R> %88-d\n", 12);
-	ft_printf("Y> %18.12.13.17f\n", 123.4);	
-	printf("R> %18.12.13.17f\n", 123.4);
-	ft_printf("Y> %019+#X\n", 256);	
-	printf("R> %019+#X\n", 256);	
-}
-
 void ft_brute_force_ldble(long double ref)
 {
-	int		next = 0;
 	char *flags;
 	char *ret;
 	int flags_num = 0;
 	int flags_cpy = 0;
 	long double flt;
+	// unsigned int arg = 42;
 	char *type = "Lf";
 	void *arg;
 	int i;
@@ -88,7 +34,7 @@ void ft_brute_force_ldble(long double ref)
 	int random;
 
 	flt = ref;
-	printf("\n\033[35m==================== TESTING SPECIFIER {%s} ================\033[0m\n", type);
+	printf("===================== TESTING TYPE > %s ====================\n", type);
 	i = 5;
 	ret = ft_memaset(0, 1024);
 	flags = ft_strdup(" +-#0");
@@ -126,17 +72,14 @@ void ft_brute_force_ldble(long double ref)
 		c = ft_printf(ret, flt);
 		o = printf(ret, flt);
 		printf("DIFF >> %s\n", (c == o) ? "\033[32mOK\033[0m" : "\033[31mKO\033[0m");
-		if (next == 0 && getchar() == 'n')
-			next = 1;
-		if (next == 1 && flags_num >= 5 && getchar())
-			next = 0;
+		getchar();
 		i++;
+		// printf("%d", rand() % (10 + 1 - 1) + 1);
 	}
 }
 
 void ft_brute_force(char *type)
 {
-	int		next = 0;
 	char *flags;
 	char *ret;
 	int flags_num = 0;
@@ -148,7 +91,7 @@ void ft_brute_force(char *type)
 	int c, o;
 	int random;
 
-	printf("\n\033[35m==================== TESTING SPECIFIER {%s} ================\033[0m\n", type);
+	printf("===================== TESTING TYPE > %s ====================\n", type);
 	i = 5;
 	ret = ft_memaset(0, 1024);
 	flags = ft_strdup(" +-#0");
@@ -232,14 +175,10 @@ void ft_brute_force(char *type)
 			o = printf(ret, random);
 			printf("DIFF >> %s\n", (c == o) ? "\033[32mOK\033[0m" : "\033[31mKO\033[0m");
 		}
-		if (next == 0 && getchar() == 'n')
-			next = 1;
-		if (next == 1 && flags_num >= 5 && getchar())
-			next = 0;
+		getchar();
 		i++;
 		// printf("%d", rand() % (10 + 1 - 1) + 1);
 	}
-	// getchar();
 }
 
 int main(void)
@@ -265,10 +204,6 @@ int main(void)
 	ft_brute_force("c");
 	ft_brute_force("p");
 	ft_brute_force("s");
-	printf("\n\n\n\n\033[35m=============== BONUS ===========\033[0m\n\n\n");
-	getchar();
-	getchar();
-	bonus();	
 	// ft_printf("%Lf\n", 0.01/1.0L);
 	// ft_printf("%Lf\n", 0.01/1.0L);
 	// double a = 123.4;
